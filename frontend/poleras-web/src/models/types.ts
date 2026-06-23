f83b5e10b633479c05193bb55e3d32a4415e724b
Soysakiaytuno <leonardhopst@gmail.com>
2026-06-23 16:46:36 -0400

    Inicializacion del frontend

---
+++ frontend/poleras-web/src/models/types.ts
@@ -0,0 +1,28 @@
+export interface Category {
+    id: string;
+    name: string;
+    slug: string;
+}
+
+export interface Tshirt {
+    id: string;
+    categoryid: string;
+    name: string;
+    description: string;
+    base_price: string;
+    is_active: boolean;
+}
+
+export interface TshirtSize {
+    id: string;
+    tshirt_id: string;
+    size_label: string;
+    is_available: boolean;
+}
+
+export interface PresetDesing {
+    id: string;
+    name: string;
+    is_active: boolean;
+    image: string;
+}
